c53a83143c62d6b01e7a45bb237271b44bb7e49f
JesCar-Gar <jesusalexandercara07@gmail.com>
2026-02-08 17:55:12 -0400

    falta base de datos

---
+++ backend.php
@@ -0,0 +1,12 @@
+<?php
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$correo = $_POST["correo"];
+$genero = $_POST["genero"];
+
+echo "<p><strong>nombre:</strong> $nombre</p><br>";
+echo "<p><strong>apellido:</strong> $apellido</p><br>";
+echo "<p><strong>correo:</strong> $correo</p><br>";
+echo "<p><strong>genero:</strong> $genero</p><br>";
+echo "<p>gracias por la informacion </p><br>";
+?>
